65d475ca5a658709a1f7edcd6358083d94bd1577
ali <ali.saranj1384@gmail.com>
2024-06-29 10:08:22 +0000

    WEB-5 create all App

--- shopApp/settings.py
+++ shopApp/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "shop.apps.ShopConfig",
+    "shoping.apps.ShopingConfig",
+    "product.apps.ProductConfig",
+    "api.apps.ApiConfig"
 ]
 
 MIDDLEWARE = [
