0bb6c55c02083dc49e7742af959af0b0a4cd02e6
ali <ali.saranj1384@gmail.com>
2024-06-29 12:19:26 +0000

    create model create admin

--- shopApp/settings.py
+++ shopApp/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     "shop.apps.ShopConfig",
     "shoping.apps.ShopingConfig",
     "product.apps.ProductConfig",
-    "api.apps.ApiConfig"
+    "api.apps.ApiConfig",
+    "user.apps.UserConfig"
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
