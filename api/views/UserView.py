ac2b99f92c3d9404927b8bbcc033955176e54f19
delta <amin138400138400@gmail.com>
2024-07-01 15:56:48 +0330

    Create_Api_order

--- api/views/UserView.py
+++ api/views/UserView.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from rest_framework import status
 from api.serializer.UserSerializer import UserSerializer
+from user.models import Customer
+
 
 # Register user
 @api_view(['POST'])
@@ -15,15 +17,20 @@ def Register_user(request):
         return Response(user.data, status=status.HTTP_201_CREATED)
     return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 # Login user
 @api_view(['POST'])
-def login_user(request):
+def Login_user(request):
     username = request.data.get('username')
     password = request.data.get('password')
-    user = authenticate(request, username=username, password=password)
+
+    user = authenticate(username=username, password=password)
 
     if user is not None:
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key}, status=status.HTTP_200_OK)
-    else:
-        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            customer = Customer.objects.get(username=user)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        except Customer.DoesNotExist:
+            return Response({'error': 'Customer profile not found'}, status=status.HTTP_404_NOT_FOUND)
+    return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
